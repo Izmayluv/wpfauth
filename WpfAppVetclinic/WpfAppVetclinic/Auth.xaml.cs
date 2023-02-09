a644163f26c9ebf3cb0cb8ce1ccc5284bc476efc
1s <91906602+Izmayluv@users.noreply.github.com>
2023-02-09 15:49:12 +0300

    wpf

---
+++ WpfAppVetclinic/WpfAppVetclinic/Auth.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppVetclinic
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Auth : Window
+    {
+        public Auth()
+        {
+            InitializeComponent();            
+        }
+
+        private void buttonReCaptcha_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+
